dee1f9107fd24a4ba7bb37af136c47148daac723
drengual <drengual17@gmail.com>
2025-06-03 12:28:28 +0800

    remove context chat function

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { ChatProvider } from "@/contexts/ChatContext";
 import { ThemeProvider, useTheme } from "next-themes";
 import React from "react";
 import { Toaster } from "sonner";
-import Chat from "./Chat";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
